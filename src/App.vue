e89b0a8e53e958024a8e3d517ec158fd14b6873b
Bruno <b-d.dupuis@laposte.net>
2026-06-11 13:04:09 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <h1>Test Admin avec Supabase</h1>
+
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla eligendi inventore, sunt nam
+      perspiciatis dolores cupiditate saepe ut mollitia officia laborum facere molestias quae in.
+      Neque distinctio expedita veritatis voluptas.
+    </p>
+  </div>
+  <div>
+    <img src="../public/images/1.jpg" alt="logo artisan" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image {
+  margin: 0 auto;
+  width: 500px;
+}
+h1 {
+  color: blueviolet;
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+
+div {
+  margin: 0 auto;
+  border: 2px red solid;
+  width: 500px;
+}
+</style>
