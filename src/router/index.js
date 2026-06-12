b24f28a3d209f1883879b815734006a1df0e3137
Bruno <b-d.dupuis@laposte.net>
2026-06-13 01:05:50 +0200

    ninth

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { supabase } from '../supabase'
 
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Admin from '../views/Admin.vue'
 
 const routes = [
-  {
-    path: '/',
-    component: Home,
-  },
-  {
-    path: '/login',
-    component: Login,
-  },
-  {
-    path: '/admin',
-    component: Admin,
-  },
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/admin', component: Admin, meta: { requiresAuth: true } },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+router.beforeEach(async (to, from, next) => {
+  const { data } = await supabase.auth.getUser()
+
+  const user = data.user
+
+  if (to.meta.requiresAuth && !user) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
