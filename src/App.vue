a00e9b82726f110dccb155f4de6a1bdc40714e0b
Bruno <b-d.dupuis@laposte.net>
2026-06-13 01:37:30 +0200

    six

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
   <router-view />
 </template>
-<script>
-import { onMounted } from 'vue'
-import { supabase } from './supabase'
-
-onMounted(async () => {
-  await supabase.auth.getSession()
-})
-</script>
